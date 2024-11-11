b36aad135f2b8666464608c7486ec81f3e25126c
Rafael Haenel <rafaelhaenel@phas.ubc.ca>
2024-11-11 20:34:39 +0100

    Update commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,12 @@
 import { commands, ExtensionContext } from "vscode";
 import { FlamegraphPanel } from "./panels/FlamegraphPanel";
+import * as vscode from "vscode";
+import {
+  loadProfileCommand,
+  runProfilerCommand,
+  toggleProfileCommand,
+} from "./commands";
+import { unregisterProfile } from "./register";
 
 export function activate(context: ExtensionContext) {
   // Create the show hello world command
@@ -10,6 +17,20 @@ export function activate(context: ExtensionContext) {
     }
   );
 
-  // Add command to the extension context
-  context.subscriptions.push(showFlamegraphCommand);
+  console.log("Activated");
+  context.workspaceState.update("profileVisible", false);
+
+  // Register all commands
+  context.subscriptions.push(
+    loadProfileCommand(context),
+    toggleProfileCommand(context),
+    runProfilerCommand(context),
+    showFlamegraphCommand,
+  );
+}
+
+// This method is called when your extension is deactivated
+export function deactivate(context: vscode.ExtensionContext) {
+  // Reset the lineDecorationsWidth when the extension is deactivated
+  unregisterProfile(context);
 }
