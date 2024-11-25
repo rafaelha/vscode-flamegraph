9c8af89a5d4e61fb405871681ccd7d9a28fe58af
Rafael Haenel <rafaelhaenel@phas.ubc.ca>
2024-11-24 17:11:42 -0800

    Remove modern color palette and related function from utilities

--- src/utilities/colors.ts
+++ src/utilities/colors.ts
@@ -1,29 +1,5 @@
 import { getModuleName } from './getUri';
 
-const modernColorPalette: string[] = [
-    '#FF6F61', // Coral
-    '#6B5B95', // Purple
-    '#88B04B', // Green
-    '#92A8D1', // Light Blue
-    '#955251', // Mauve
-    '#B565A7', // Orchid
-    '#009B77', // Teal
-    '#DD4124', // Red
-    '#D65076', // Raspberry
-    '#45B8AC', // Turquoise
-    '#5B5EA6', // Indigo
-    '#9B2335', // Maroon
-    '#55B4B0', // Aqua
-    '#E15D44', // Tomato
-    '#BC243C', // Crimson
-    '#C3447A', // Fuchsia
-];
-
-// Function to get a color by index
-export function getColorByIndex(index: number): string {
-    return modernColorPalette[index % modernColorPalette.length];
-}
-
 export function hashString(str: string): number {
     let hash = 0x9e3779b9; // Initial seed value for the hash function
     for (let i = 0; i < str.length; i++) {
